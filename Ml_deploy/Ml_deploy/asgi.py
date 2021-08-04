f14e874f7c3155b6bdcbadaef69d7c4054a5a468
bkablam11 <bkablam11@gmail.com>
2021-08-04 21:08:44 +0000

    update

---
+++ Ml_deploy/Ml_deploy/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Ml_deploy project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ml_deploy.settings')
+
+application = get_asgi_application()
---
+++ Ml_deploy/bankingapps/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BankingappsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bankingapps'
