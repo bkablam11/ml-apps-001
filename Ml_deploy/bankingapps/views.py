22b97544f98a53e6a203d630138f1bc56b2dc0f9
bkablam11 <bkablam11@gmail.com>
2021-08-04 21:41:59 +0000

    update

--- Ml_deploy/bankingapps/views.py
+++ Ml_deploy/bankingapps/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    context ={'a':'<h1>Hello HttpResponse</h1>'}
+    return render(request, 'index.html', context)
